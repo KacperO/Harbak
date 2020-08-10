47d5e3aabf7cbba7c21b93170285ce3ac2e9faf9
KacperO <kacper.kowalski1995@gmail.com>
2020-08-10 19:39:08 +0200

    Updates

---
+++ precache-manifest.ac61b72510c023d97b4c027b923cc70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf45cd2e910f30f890e8c864e9e402f",
+    "url": "/Harbak/index.html"
+  },
+  {
+    "revision": "f63ec896f95a450bb356",
+    "url": "/Harbak/static/css/main.4eb2820f.chunk.css"
+  },
+  {
+    "revision": "02829232a60566ef86ce",
+    "url": "/Harbak/static/js/2.808ce714.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/Harbak/static/js/2.808ce714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63ec896f95a450bb356",
+    "url": "/Harbak/static/js/main.4d2b8a6c.chunk.js"
+  },
+  {
+    "revision": "5d9b02da2c241a258f04",
+    "url": "/Harbak/static/js/runtime-main.bfac53f3.js"
+  }
+]);
